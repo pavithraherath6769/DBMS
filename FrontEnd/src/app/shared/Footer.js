c03d16c749f88d1f145f03185bd8cc1cd931cbb1
pavithraherath <pavithraherath6769@gmail.com>
2024-03-03 13:10:50 +0530

    name changed

--- FrontEnd/src/app/shared/Footer.js
+++ FrontEnd/src/app/shared/Footer.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-  render () {
+  render() {
     return (
       <footer className="footer">
         <div className="d-sm-flex justify-content-center justify-content-sm-between py-2">
-          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.tetherfi.com/" target="_blank" rel="noopener noreferrer">tetherfi.com </a>2022</span>
-          <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Provided by <a href="https://www.tetherfi.com/" target="_blank" rel="noopener noreferrer"> Tetherfi SL </a> </span>
+          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.sliit.lk/" target="_blank" rel="noopener noreferrer">SLIIT</a>2024</span>
+          <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Provided by <a href="https://www.sliit.lk/" target="_blank" rel="noopener noreferrer"> SLIIT </a>2024 </span>
         </div>
-      </footer> 
+      </footer>
     );
   }
 }
--- FrontEnd/src/app/user-pages/Login.js
+++ FrontEnd/src/app/user-pages/Login.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Link ,useHistory} from 'react-router-dom';
-import { Form ,Button } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 
-const Login =()=> {
-  
+const Login = () => {
+
   const history = useHistory();
   const onFormSubmit = e => {
     try {
       e.preventDefault()
       const formData = new FormData(e.target),
-      formDataObj = Object.fromEntries(formData.entries())
+        formDataObj = Object.fromEntries(formData.entries())
       console.log(formDataObj)
-      history.push({pathname: "/console",state: { user : formDataObj.userName }});
+      history.push({ pathname: "/console", state: { user: formDataObj.userName } });
     } catch (error) {
-      console.error("Login",error);
+      console.error("Login", error);
     }
-    
+
   }
   return (
     <div className="container-scroller">
       <div className="container-fluid page-body-wrapper full-page-wrapper">
         <div className="main-panel">
-          <div className="content-wrapper">          
+          <div className="content-wrapper">
             <div>
               <div className="d-flex align-items-center auth px-0">
                 <div className="row w-100 mx-0">
@@ -30,11 +30,11 @@ const Login =()=> {
                       <div className="brand-logo">
                         <img src={require("../../assets/images/logo.svg")} alt="logo" />
                       </div>
-                      <h4>Hello! let's get started</h4>
+                      <h4>Welcome to DBMS login Page</h4>
                       <h6 className="font-weight-light">Sign in to continue.</h6>
                       <Form className="pt-3" onSubmit={onFormSubmit}>
                         <Form.Group className="d-flex search-field">
-                          <Form.Control required type="email" name="userName" placeholder="Username"  size="lg" className="h-auto" />
+                          <Form.Control required type="email" name="userName" placeholder="Username" size="lg" className="h-auto" />
                         </Form.Group>
                         <Form.Group className="d-flex search-field">
                           <Form.Control required type="password" name="password" placeholder="Password" size="lg" className="h-auto" />
@@ -46,21 +46,21 @@ const Login =()=> {
                         <div className="my-2 d-flex justify-content-between align-items-center">
                           <div className="form-check">
                             <label className="form-check-label text-muted">
-                              <input style={{cursor:"not-allowed"}} type="checkbox" className="form-check-input"/>
+                              <input style={{ cursor: "not-allowed" }} type="checkbox" className="form-check-input" />
                               <i className="input-helper"></i>
                               Keep me signed in
                             </label>
                           </div>
-                          <a style={{cursor:"not-allowed"}} href="!#" onClick={event => event.preventDefault()} className="auth-link text-black">Forgot password?</a>
-                        </div>                        
-                        <div  className="text-center mt-4 font-weight-light">
-                          Don't have an account? <Link style={{cursor:"not-allowed"}} to="/user-pages/register" className="text-primary">Create</Link>
+                          <a style={{ cursor: "not-allowed" }} href="!#" onClick={event => event.preventDefault()} className="auth-link text-black">Forgot password?</a>
+                        </div>
+                        <div className="text-center mt-4 font-weight-light">
+                          Don't have an account? <Link style={{ cursor: "not-allowed" }} to="/user-pages/register" className="text-primary">Create</Link>
                         </div>
                       </Form>
                     </div>
                   </div>
                 </div>
-              </div>  
+              </div>
             </div>
           </div>
         </div>
